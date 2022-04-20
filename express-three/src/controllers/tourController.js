0d042a83cc3f9713c0683f5bca08c684fd8ee19f
vatsal <vatsal.patel@esparkinfo.com>
2022-04-20 20:22:11 +0530

    route param practic added

--- express-three/src/controllers/tourController.js
+++ express-three/src/controllers/tourController.js
@@ -1,9 +1,53 @@
 import fs from 'fs';
-import fsAsync from 'fs/promises';
+// for node 12 +
+// import fsAsync from 'fs/promises';
+
+// for node 12
+const fsAsync = fs.promises;
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../data/tours.json`));
 
-const getaAllTours = (req, res) => {
+// 2
+// before creating that params check middleware lets understand difference between res.json(), next() and return
+
+// - res.json() = as you know this many other response methods as used to send response to clients but the key thing to
+// note hear is when we do this the response is sent to client that mins we can't process the request further and we can'r
+// modify or work on the request but if we will have something else in code in controller after that then it will still run
+// so that mins the response will be sent but it not stop code execution further so if we have other code it will still run
+// but if we try to manipulation the request after res.json then we will get err so code execution is allowed but
+// request manipulation and resend response is not allowed after sending response once because response is sent already
+
+// - return = hear return will work just like it did in simple function that mins any of code after that will not run
+// so if we have even clg after return in controller it will not work
+// so if we do return return res.json() then it will send response ad then stop any execution after that
+
+// - next() = next is as you know it's useful for just passing request further from middleware so it can continue
+// in middleware stack that mins any code in middleware will not execute after next()
+
+// now lets use and understand this 3 things in checkId middleware
+
+const checkId = (req, res, next, val) => {
+  const id = Number(req.params.id);
+
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    // hear we want to send the response 404 so we are using the res.json but also notice that after that
+    // because resource is not found so we should not continue in this middleware and also we have to break
+    // middleware stack so request not go further so we also use return with res.json so it stop execution
+    // of current request and also break middleware stack
+    return res.status(404).json({
+      status: 'fail',
+      message: `tour with ${id} not found.`,
+    });
+  }
+
+  // hear if we id find the resource and so we will not go in if(!tour) check so request should continue further in
+  // middleware stack so we did next hear so request can continue further.
+  next();
+};
+
+const getAllTours = (req, res) => {
   res.json({
     status: 'success',
     data: { tours },
@@ -31,18 +75,7 @@ const createTour = async (req, res) => {
   });
 };
 
-const getSingelTour = (req, res) => {
-  const id = Number(req.params.id);
-
-  const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    res.status(404).json({
-      status: 'fail',
-      message: `tour with ${id} not found.`,
-    });
-  }
-
+const getSingleTour = (req, res) => {
   res.json({
     status: 'success',
     data: { tour },
@@ -75,10 +108,11 @@ const deleteTour = (req, res) => {
 };
 
 export {
-  getaAllTours,
-  getSingelTour,
+  getAllTours,
+  getSingleTour,
   createTour,
   updateTourByPut,
   updateTourByPatch,
   deleteTour,
+  checkId,
 };
--- express-three/src/routes/tourRouter.js
+++ express-three/src/routes/tourRouter.js
@@ -1,21 +1,47 @@
 import express from 'express';
 
 import {
-  getaAllTours,
+  getAllTours,
   createTour,
-  getSingelTour,
+  getSingleTour,
   updateTourByPut,
   updateTourByPatch,
   deleteTour,
 } from 'controllers/tourController';
+import { checkId } from '../controllers/tourController';
 
 const router = express.Router();
 
-router.get('/', getaAllTours);
+// 1
+// there is one special kind of middleware which is provided by the router object cald parm middleware this middleware will
+// except one dynamic param name and this middleware will call when this param will be passed in any routs
+// hear this param will execute when we will get id in put, patch, delete request in tours resource and on there
+// endpoints as well which we will create in future which will have id as dynamic param in it.
+// this param will have req, res, next as usual middleware but it will also have one extra param which will be dynamic value
+// for which we are registering this middleware hear it will be dynamic id.
+
+// this param middleware is useful when we want to execute one check that id is available in db or not and if not
+// then return 404 or else continue in middleware stack so we don't have to check in each and every routs for
+// this check repetitively
+
+// now you will argue that we can also create separate function to check this and can call that function in all routs
+// where we want to check this condition but for that we also have to call that function in all routes and according
+// to express rules all checks of this kind should do by middleware stack only. so in controllers we will only
+// have logic for that those controllers are created in get delete etc controllers we just get or delete resource
+// as the controller name suggest we try to avoid that extra checks in controllers so every controller is responsible
+// for single task. now lets create one controller to check if id is exist and then use it in this id param
+router.param('id', (req, res, next, val) => {
+  console.log(`hit tour id param middleware : ${val}`);
+  next();
+});
+
+router.param('id', checkId);
+
+router.get('/', getAllTours);
 
 router.post('/', createTour);
 
-router.get('/:id', getSingelTour);
+router.get('/:id', getSingleTour);
 
 router.put('/:id', updateTourByPut);
 
